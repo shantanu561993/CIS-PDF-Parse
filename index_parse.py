4df54bd9dc0c2d7e03b68cb95f7f4a75ff209b95
Shantanu Khandelwal <shantanu561993@gmail.com>
2019-05-07 20:53:37 +0800

    Initial Upload

---
+++ index_parse.py
@@ -0,0 +1,12 @@
+import re
+index=open("index.txt","r").readlines()
+for line in index:
+    try:
+        match =  re.match("((\d\.)+\d*\s+.+\((Not\sScored|Scored)\))",line)
+        if match:
+            data =  match.group()
+            print data
+            #match1 = re.match("(((\d\.)+)\d*)",data)
+            #print '"'+match1.group()+'",',
+    except:
+        pass
